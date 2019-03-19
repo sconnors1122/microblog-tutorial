ab924f11cb5e68ff4c5f581f53e8bab16b322f22
Shannon <sconnors1122@gmail.com>
2019-03-19 12:14:44 -0400

    Chapters 2 and 3

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from app import app
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+from flask import render_template, flash, redirect, url_for
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
